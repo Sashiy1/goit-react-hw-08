a52123de7cf234af40192b9277d0e2e8c6437aa0
Sashiy1 <aleksander.paluch@wp.pl>
2024-03-22 13:35:37 +0100

    initial commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,11 @@
 import "./App.css";
-import Profile from "../Profile/Profile.jsx";
-import { userData } from "../dataJson/userData.json";
-import FriendList from "../Friends/FriendList.jsx";
-import friends from "../dataJson/friendsData.json";
-import TransactionHistory from "../TransactionHistory/TransactionHistory.jsx";
-import transactions from "../dataJson/transactions.json"
+
 
 const App = () => {
 
   return (
     <>
-      <Profile
-        name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}
-      />
-     <FriendList friends={friends} />
-     <TransactionHistory items={transactions} />
+ 
     </>
   );
 };
