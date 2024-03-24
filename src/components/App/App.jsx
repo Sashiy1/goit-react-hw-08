fd02df64c029dfd92b9530e0ee538691fecea9d8
Sashiy1 <aleksander.paluch@wp.pl>
2024-03-24 15:22:53 +0100

    done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,68 @@
 import "./App.css";
-
+import { useState } from "react";
+import { useEffect } from "react";
+import Description from "../Description/Description";
+import Options from "../Options/Options";
+import Feedback from "../Feedback/Feedback";
+import Notification from "../Notification/Notification";
 
 const App = () => {
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => {
+      const newFeedback = { ...prevFeedback };
+      newFeedback[feedbackType] = prevFeedback[feedbackType] + 1;
+      return newFeedback;
+    });
+  };
+
+  const resetFeedback = () => {
+    setFeedback({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+
+    setShowFeedback(false);
+  };
+
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = window.localStorage.getItem("savedFeedback");
+
+    if (savedFeedback !== null) {
+      return JSON.parse(savedFeedback);
+    }
+
+    return { good: 0, neutral: 0, bad: 0 };
+  });
+
+  const [showFeedback, setShowFeedback] = useState(false);
+
+  useEffect(() => {
+    handleShowFeedback();
+    window.localStorage.setItem("savedFeedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
+  const handleShowFeedback = () => {
+    if (totalFeedback > 0) {
+      setShowFeedback((prevState) => true);
+    }
+  };
 
   return (
     <>
- 
+      <Description />
+      <Options
+        handleClick={updateFeedback}
+        totalFeedback={totalFeedback}
+        handleReset={resetFeedback}
+      />
+      {showFeedback ? (
+        <Feedback feedback={feedback} totalFeedback={totalFeedback} />
+      ) : (
+        <Notification />
+      )}
     </>
   );
 };
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,7 @@
+const Notification = () => {
+    return (
+      <p style={{ fontSize: '20px' }}>No feedback yet</p>
+    );
+  };
+  
+  export default Notification;
