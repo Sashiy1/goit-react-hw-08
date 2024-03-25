c93f88ef04cfbd5b99ba7e3f1e89945be815b6ec
Sashiy1 <aleksander.paluch@wp.pl>
2024-03-26 00:46:52 +0100

    initial

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,59 +1,11 @@
 import "./App.css";
 import { useState } from "react";
 import { useEffect } from "react";
-import Description from "../Description/Description";
-import Options from "../Options/Options";
-import Feedback from "../Feedback/Feedback";
-import Notification from "../Notification/Notification";
 
 const App = () => {
-  const updateFeedback = (feedbackType) => {
-    setFeedback((prevFeedback) => {
-      const newFeedback = { ...prevFeedback };
-      newFeedback[feedbackType] = prevFeedback[feedbackType] + 1;
-      return newFeedback;
-    });
-  };
-
-  const resetFeedback = () => {
-    setFeedback({
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    });
-  };
-
-  const [feedback, setFeedback] = useState(() => {
-    const savedFeedback = window.localStorage.getItem("savedFeedback");
-
-    if (savedFeedback !== null) {
-      return JSON.parse(savedFeedback);
-    }
-
-    return { good: 0, neutral: 0, bad: 0 };
-  });
-
-  useEffect(() => {
-    window.localStorage.setItem("savedFeedback", JSON.stringify(feedback));
-  }, [feedback]);
-
-  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
-
-  const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100)
-  
   return (
     <>
-      <Description />
-      <Options
-        handleClick={updateFeedback}
-        totalFeedback={totalFeedback}
-        handleReset={resetFeedback}
-      />
-      {totalFeedback !== 0 ? (
-        <Feedback feedback={feedback} totalFeedback={totalFeedback} positiveFeedback={positiveFeedback} />
-      ) : (
-        <Notification />
-      )}
+      <h1>Phonebook</h1>
     </>
   );
 };
--- src/components/Notification/Notification.jsx
+++
@@ -1,7 +0,0 @@
-const Notification = () => {
-    return (
-      <p style={{ fontSize: '20px' }}>No feedback yet</p>
-    );
-  };
-  
-  export default Notification;
